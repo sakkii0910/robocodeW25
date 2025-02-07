a50e58ea1091b61ed5082104b289eeb40b12ab29
pranay <pranaypatel1707.pvp@gmail.com>
2025-02-07 11:19:57 -0500

    #116 Block of commented-out lines removed.

--- robocode.api/src/main/java/net/sf/robocode/serialization/RbSerializer.java
+++ robocode.api/src/main/java/net/sf/robocode/serialization/RbSerializer.java
@@ -225,9 +225,6 @@ public final class RbSerializer {
 		} else {
 			buffer.put(TERMINATOR_TYPE);
 		}
-		// FOR-DEBUG if (expect != buffer.position()) {
-		// FOR-DEBUG 	throw new Error("Bad size");
-		// FOR-DEBUG }
 	}
 
 	public void serialize(ByteBuffer buffer, String data) {
