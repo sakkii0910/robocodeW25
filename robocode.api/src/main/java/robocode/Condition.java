5a049e371d90887c6ad0ec66208078a7f726f0ca
Abhishek Maurya <abhishek2504maurya@gmail.com>
2025-02-07 16:20:31 -0500

    Fixed constructor visibility in Condition.java

--- robocode.api/src/main/java/robocode/Condition.java
+++ robocode.api/src/main/java/robocode/Condition.java
@@ -50,7 +50,7 @@ public abstract class Condition {
 	/**
 	 * Creates a new, unnamed Condition with the default priority, which is 80.
 	 */
-	public Condition() {}
+	protected Condition() {}
 
 	/**
 	 * Creates a new Condition with the specified name, and default priority,
@@ -58,7 +58,7 @@ public abstract class Condition {
 	 *
 	 * @param name the name for the new Condition
 	 */
-	public Condition(String name) {
+	protected Condition(String name) {
 		this.name = name;
 	}
 
